7fc4765da24c20ddfc276b9747862eadc7c182fa
metjush <metjush@gmail.com>
2016-05-11 15:36:06 +0200

    start FE file

---
+++ src/fixed_effects.py
@@ -0,0 +1,19 @@
+"""
+Fixed Effects for Panel Data
+"""
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+class FixedEffects(object):
+    def __init__(self, panel, y, x):
+        """
+
+        :param panel:
+        :param y:
+        :param x:
+        :return:
+        """
+
+        pass
