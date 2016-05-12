583838b481773cdeaeba0f96fff3ef4c8c8872b9
metjush <metjush@gmail.com>
2016-05-12 09:51:04 +0200

    Test the FE implementation

--- src/tests.py
+++ src/tests.py
@@ -1,5 +1,6 @@
 import cPickle as pickle
 from first_diff import FirstDiff
+from fixed_effects import FixedEffects
 import pandas as pd
 
 data_file = '../data/nulled_panel'
@@ -10,3 +11,9 @@ if __name__ == '__main__':
     print(panel)
     fd = FirstDiff(panel, 'SP.DYN.LE00.IN', ['SH.XPD.PRIV.ZS', 'SH.XPD.PUBL.ZS', 'SH.XPD.PUBL.GX.ZS', 'SH.XPD.PUBL', 'SH.XPD.PCAP.PP.KD', 'SH.XPD.TOTL.ZS', 'SH.MED.BEDS.ZS'])
     fd.estimate()
+
+    fe = FixedEffects(panel, 'SP.DYN.LE00.IN', ['SH.XPD.PRIV.ZS', 'SH.XPD.PUBL.ZS', 'SH.XPD.PUBL.GX.ZS', 'SH.XPD.PUBL', 'SH.XPD.PCAP.PP.KD', 'SH.XPD.TOTL.ZS', 'SH.MED.BEDS.ZS'])
+    fe.estimate()
+
+    fe2 = FixedEffects(panel, 'SP.DYN.LE00.IN', ['SH.XPD.PRIV.ZS', 'SH.XPD.PUBL.ZS', 'SH.XPD.PUBL.GX.ZS', 'SH.XPD.PUBL', 'SH.XPD.PCAP.PP.KD', 'SH.XPD.TOTL.ZS', 'SH.MED.BEDS.ZS'], True)
+    fe2.estimate()
